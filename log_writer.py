05f201e4c14f04e39a43683875073887b8a17804
benrconway84 <benrconway84@gmail.com>
2018-06-23 20:05:17 +0100

    Added two other SQL queries, may need debug

--- log_writer.py
+++ log_writer.py
@@ -9,10 +9,9 @@ conn = psycopg2.connect("dbname=news")
 
 # Open a cursor to perform database operations
 cur = conn.cursor()
-sql = "select title, num from articles, (select substring(path from 10) as short, count(*) as num from log where path <> '/' and status = '200 OK' group by path order by num desc limit 3) as favs where articles.slug = favs.short order by num desc;"
+
 # Execute a command: this creates a new table
 cur.execute(sql)
-
 
 # Query the database and obtain data as Python objects
 results = cur.fetchall()
@@ -31,12 +30,14 @@ conn.close()
 # SQL querying code here
 
 # SQL query to answer question 1 without views
-
+sql = "select title, num from articles, (select substring(path from 10) as short, count(*) as num from log where path <> '/' and status = '200 OK' group by path order by num desc limit 3) as favs where articles.slug = favs.short order by num desc;"
 # SQL query to answer question 2 without views
+
+sql2 = "select authors.name, favs.num from authors, articles,(select substring(path from 10) as short, count(*) as num from log where path <> '/' and status = '200 OK' group by path order by num desc) as favs where articles.slug = favs.short and articles.author = authors.id order by num desc;"
 
 # SQL query to answer question 3 without views
 
-
+sql3 = "select a.date, round(a.num*100/b.num::numeric, 2) as percent from (select date, count(date) as num from (select time::timestamp::date as date from log where status <> '404 NOT FOUND') as a group by date order by date desc) as a, (select date, count(date) as num from (select time::timestamp::date as date from log where status = '200 OK') as a group by date order by date desc) as b where a.date = b.date;"
 
 
 
