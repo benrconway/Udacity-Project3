3d40bf4446f010c5d0e4f749a05e2da2433c9d4d
benrconway84 <benrconway84@gmail.com>
2018-06-27 20:59:44 +0100

    checkout output

--- log_writer.py
+++ log_writer.py
@@ -13,43 +13,37 @@ def query(sql_query):
     return results
 
 def topThree(results):
-    print("The Top Three Articles of All Time")
+    print("The top three articles of all time:")
     for result in results:
         print("'{}' -- {} views".format(result[0], result[1]))
-    #
-    # print("'%s' -- %d views" % (results[1][0], results[1][1]))
-    # print("'%s' -- %d views" % (results[2][0], results[2][1]))
 
+def topAuthors(results):
+    print("The most popular authors of all time:")
+    for result in results:
+        print("'{}' -- {} views".format(result[0], result[1]))
 
-# Connect to an existing database
+def topErrors(results):
+    print("The days on which more than 1\% of requests lead to errors:")
+    for result in results:
+        print("'{}' -- {} views".format(result[0], result[1]))
 
-# Open a cursor to perform database operations
+def
 
-# Execute a command: this creates a new table
-
-# Query the database and obtain data as Python objects
-
-# Make the changes to the database persistent
-
-# Close communication with the database
-
-
-# SQL querying code here
-
-# SQL query to answer question 1 without views
-sql = "select title, num from articles, (select substring(path from 10) as short, count(*) as num from log where path <> '/' and status = '200 OK' group by path order by num desc) as favs where articles.slug = favs.short order by num desc;"
-# SQL query to answer question 2 without views
-
+# SQL query to answer question 1
+sql = "select title, num from articles, (select substring(path from 10) as short, count(*) as num from log where path <> '/' and status = '200 OK' group by path order by num desc limit 3) as favs where articles.slug = favs.short order by num desc;"
+# SQL query to answer question 2
 sql2 = "select authors.name, favs.num from authors, articles,(select substring(path from 10) as short, count(*) as num from log where path <> '/' and status = '200 OK' group by path order by num desc) as favs where articles.slug = favs.short and articles.author = authors.id order by num desc;"
-
-# SQL query to answer question 3 without views
-
+# SQL query to answer question 3
 sql3 = "select a.date, round(a.num*100/b.num::numeric, 2) as percent from (select date, count(date) as num from (select time::timestamp::date as date from log where status <> '404 NOT FOUND') as a group by date order by date desc) as a, (select date, count(date) as num from (select time::timestamp::date as date from log where status = '200 OK') as a group by date order by date desc) as b where a.date = b.date;"
 
 results1 = query(sql)
-print(results1)
 topThree(results1)
 
+results2 = query(sql2)
+topAuthors(results2)
+
+results3 = query(sql3)
+topErrors(results3)
 
 
 #######################################################################
