2227941b5dcb8dffb459c72c737cc369a89e9913
benrconway84 <benrconway84@gmail.com>
2018-06-27 20:52:01 +0100

    Experiment 2

--- log_writer.py
+++ log_writer.py
@@ -15,7 +15,7 @@ def query(sql_query):
 def topThree(results):
     print("The Top Three Articles of All Time")
     for result in results:
-        print("'%s' -- %d views" % (result[0], results[1]))
+        print("'{}' -- {} views".format(result[0], result[1]))
     #
     # print("'%s' -- %d views" % (results[1][0], results[1][1]))
     # print("'%s' -- %d views" % (results[2][0], results[2][1]))
@@ -37,7 +37,7 @@ def topThree(results):
 # SQL querying code here
 
 # SQL query to answer question 1 without views
-sql = "select title, num from articles, (select substring(path from 10) as short, count(*) as num from log where path <> '/' and status = '200 OK' group by path order by num desc limit 3) as favs where articles.slug = favs.short order by num desc;"
+sql = "select title, num from articles, (select substring(path from 10) as short, count(*) as num from log where path <> '/' and status = '200 OK' group by path order by num desc) as favs where articles.slug = favs.short order by num desc;"
 # SQL query to answer question 2 without views
 
 sql2 = "select authors.name, favs.num from authors, articles,(select substring(path from 10) as short, count(*) as num from log where path <> '/' and status = '200 OK' group by path order by num desc) as favs where articles.slug = favs.short and articles.author = authors.id order by num desc;"
@@ -47,7 +47,8 @@ sql2 = "select authors.name, favs.num from authors, articles,(select substring(p
 sql3 = "select a.date, round(a.num*100/b.num::numeric, 2) as percent from (select date, count(date) as num from (select time::timestamp::date as date from log where status <> '404 NOT FOUND') as a group by date order by date desc) as a, (select date, count(date) as num from (select time::timestamp::date as date from log where status = '200 OK') as a group by date order by date desc) as b where a.date = b.date;"
 
 results1 = query(sql)
-topThree(results1)
+print results1
+# topThree(results1)
 
 
 
